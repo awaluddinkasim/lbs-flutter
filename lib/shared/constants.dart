42281d88389f9fc23ac4efcc9d1336e1855c4350
ipi <stnrfzhlfnksm21@gmail.com>
2024-07-12 15:33:13 +0800

    update base url

--- lib/shared/constants.dart
+++ lib/shared/constants.dart
@@ -2,5 +2,5 @@ class Constants {
   Constants._();
 
   // static const String baseUrl = 'http://10.0.2.2:8000';
-  static const String baseUrl = 'https://locomotive21.egols.my.id';
+  static const String baseUrl = 'https://locomotive21.elnino-code.com';
 }
